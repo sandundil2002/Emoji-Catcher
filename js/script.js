a556851ab292fefd9e022a28cc3d92252acb86c6
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-05-04 12:08:00 +0530

    create an emoji array...

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let emoji1 = document.getElementById("emoji-1");
+let emoji2 = document.getElementById("emoji-2");
+let emoji3 = document.getElementById("emoji-3");
+let emoji4 = document.getElementById("emoji-4");
+let emoji5 = document.getElementById("emoji-5");
+let emoji6 = document.getElementById("emoji-6");
+let emoji7 = document.getElementById("emoji-7");
+let emoji8 = document.getElementById("emoji-8");
+let emoji9 = document.getElementById("emoji-9");
+let emoji10 = document.getElementById("emoji-10");
+
+var emojiArray = [emoji1,emoji2,emoji3,emoji4,emoji5,emoji6,emoji7,emoji8,emoji9,emoji10]
+
+
